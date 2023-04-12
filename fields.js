979e009e8800e59075d3b3db9002cac32815b5c0
dmh <5150636+dmh@users.noreply.github.com>
2023-04-12 17:03:15 +0300

    [TASK] refactor all fields, add more info and comments, add examples, add hints

--- fields.js
+++ fields.js
@@ -1,5 +1,11 @@
 import * as contentTabFields from './lib/content-tab-fields.js'
 import * as styleTabFields from './lib/style-tab-fields.js'
 import * as fields from './lib/fields.js'
+
+/**
+ * `@hubspot-fields-js`
+ * ### Module fields entry point
+ */
 const fi = { ...contentTabFields, ...styleTabFields, ...fields }
+
 export { fi }
