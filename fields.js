f90088ef78a0f8f4b1a2b99ced9c63ef369b945e
dmh <5150636+dmh@users.noreply.github.com>
2023-05-05 18:42:38 +0300

    [TASK] update jsdoc syntax to add possibility to generate documentation

--- fields.js
+++ fields.js
@@ -3,7 +3,6 @@ import * as styleTabFields from './lib/style-tab-fields.js'
 import * as fields from './lib/fields.js'
 
 /**
- * `@hubspot-fields-js`
  * ### Module fields entry point
  */
 const fi = { ...contentTabFields, ...styleTabFields, ...fields }
