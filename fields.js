d11b7d3a99e90eb545d596b85577d4b86d79f88a
dmh <5150636+dmh@users.noreply.github.com>
2023-05-06 16:52:45 +0300

    [DOC] update jsdoc comments

--- fields.js
+++ fields.js
@@ -4,6 +4,19 @@ import * as fields from './lib/fields.js'
 
 /**
  * ### Module fields entry point
+ * @constant
+ * @example
+ * import { initModule } from '@resultify/hubspot-fields-js/lib/init.js'
+ * import { fi } from '@resultify/hubspot-fields-js/fields.js'
+ * import { group, styleGroup } from '@resultify/hubspot-fields-js/lib/group.js'
+ * initModule(
+ *   styleGroup(
+ *     fi.alignment('Alignment', 'alignment')
+ *   ),
+ *   fi.link('Link', 'link')
+ *   fi.text('Text', 'text')
+ *   fi.number('Number', 'number')
+ * )
  */
 const fi = { ...contentTabFields, ...styleTabFields, ...fields }
 
